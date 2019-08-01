f5042510d2e7e4f8ffeb8a4019715b74e55ff9f8
hukaibo <52575259+hukaibo@users.noreply.github.com>
2019-08-02 07:32:52 +0800

    My branch (#35)
    My branch (#35)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -22,7 +22,13 @@ public class User {
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
     }
-
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
-
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            return this.id.equals(((User) obj).id);
+        } else {
+            return false;
+        }
+    }
 }
