4cf296e722532348d184e769fc6f93cbe74e0e3a
yueyue chou <13444339+yueyuezone@users.noreply.github.com>
2019-09-05 23:43:51 +0800

    覆盖 (#51)
    覆盖 (#51)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import java.util.Objects;
+
 public class User {
     private Integer id;
     private String name;
@@ -21,8 +23,19 @@ public class User {
         System.out.println(new User(1, "user1") == new User(1, "user1"));
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
+        System.out.println(new User(1,"user").equals(null));
+
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o==null||!(o instanceof User)) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name);
+    }
 
 }
