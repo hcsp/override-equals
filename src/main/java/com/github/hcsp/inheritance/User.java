fef6daf25815c29ee12b5b5463299d25f1995c0b
903875816 <604150318@qq.com>
2019-09-23 00:59:08 +0800

    覆盖equals方法

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.inheritance;
 
-public class User {
+import com.sun.org.apache.xpath.internal.objects.XObject;
+
+public class User{
     private Integer id;
     private String name;
 
@@ -8,13 +10,26 @@ public class User {
         this.id = id;
         this.name = name;
     }
-
     public Integer getId() {
         return id;
     }
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object Object){
+            if(Object instanceof User) {
+                User O = (User) Object;
+                if (this.id.equals(O.id)) {
+                    return true;
+                } else {
+                    return false;
+                }
+            }else{
+                return false;
+            }
     }
 
     public static void main(String[] args) {
