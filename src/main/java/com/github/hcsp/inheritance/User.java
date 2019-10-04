c1e87b935ba9934af44f22a7ab0a6904c8de41f4
wangpeng <wangpeng@icourt.cc>
2019-10-04 17:53:33 +0800

    override equals

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -29,10 +29,9 @@ public class User {
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
-        if (obj != null && obj instanceof User == true) {
-            return Objects.equals(id, obj.id);
-        }
-        return false;
+        if (obj != null && getClass() != obj.getClass()) return false;
+        User user = (User) obj;
+        return id.equals(user.id);
     }
     @Override
     public int hashCode() {
