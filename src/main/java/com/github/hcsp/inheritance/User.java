896909d5a8731cbfe06f2c816a1381bcb1e8bc91
Your Name <you@example.com>
2019-10-31 17:33:45 +0800

    override equals

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -21,8 +21,18 @@ public class User {
         System.out.println(new User(1, "user1") == new User(1, "user1"));
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
+        String str1 = new String("a");
+        String str2 = new String("b");
+        str1.equals(str2);
     }
-
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+
+    @Override
+    public boolean equals (Object user){
+        if (this.id - ((User) user).id== 0) {
+            return true;
+        }
+        return false;
+    }
 
 }
