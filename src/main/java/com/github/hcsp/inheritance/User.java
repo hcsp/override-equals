41efebab67bec1a9c419f68577561ecf8185ee82
Ke Rui <31921365+kerrypnx@users.noreply.github.com>
2019-11-01 15:15:15 +0800

    override equals (#77)
    override equals (#77)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import java.util.Objects;
+
 public class User {
     private Integer id;
     private String name;
@@ -21,8 +23,22 @@ public class User {
         System.out.println(new User(1, "user1") == new User(1, "user1"));
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
+        String str1 = new String("a");
+        String str2 = new String("b");
+        str1.equals(str2);
     }
-
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
 
+    @Override
+    public boolean equals(Object user) {
+        if (this == user) return true;
+        if (user == null || getClass() != user.getClass()) return false;
+        return Objects.equals(id, ((User) user).id) &&
+                Objects.equals(name, ((User) user).name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
