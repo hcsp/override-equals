fbc40106625fdb34149ca5fd31647e47af61a7fd
zhy533726735220 <809775352@qq.com>
2019-12-04 22:21:55 +0800

    Java面向对象初步练习：覆盖equals方法 (#86)
    Java面向对象初步练习：覆盖equals方法 (#86)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import java.util.Objects;
+
 public class User {
     private Integer id;
     private String name;
@@ -25,4 +27,18 @@ public class User {
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
 
+
+    @Override
+    public boolean equals(Object o) {
+        boolean flag = false;
+        if (o instanceof User) {
+            flag = this.id.equals(((User) o).getId());
+        }
+        return flag;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
 }
