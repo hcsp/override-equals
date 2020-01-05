671e0fe5b5dad185ff8265fb9afc49062231533b
= <>
2020-01-05 10:42:09 +0800

    Java面向对象初步练习：覆盖equals方法

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -24,5 +24,11 @@ public class User {
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+    public boolean equals(User a){
+        if(null == a) {
+            return false;
+        }
+        return a.id.equals(this.id);
+    }
 
 }
