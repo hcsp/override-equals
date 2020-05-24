fda5b7b2b2f32d3307746ac4389e74ea981abcec
hanchenhao <hanchenhao@vip.qq.com>
2020-05-24 20:21:55 +0800

    override

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -13,23 +13,26 @@ public class User {
         return id;
     }
 
-        public String getName() {
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        public boolean equals(User u) {
-            if (u == null) {
-                return false;
-            }
-            return (u.id == this.id);
-        }
 
-        public static void main(String[] args) {
+
+    public static void main(String[] args) {
             System.out.println(new User(1, "user1") == new User(1, "user1"));
-        System.out.println(new User(1, "user1").equals(new User(1, "user1")));
-        System.out.println(new User(1, "user1").equals(new User(2, "user2")));
+            System.out.println(new User(1, "user1").equals(new User(1, "user1")));
+            System.out.println(new User(1, "user1").equals(new User(null, "user2")));
+            System.out.println(new User(1, "user1").equals(null));
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
-
+    @Override
+    public boolean equals(Object u) {
+        if (u == null) return false;
+        if (u instanceof User){
+            return this.id == ((User) u).id;
+        }
+        return super.equals(u);
+    }
 }
