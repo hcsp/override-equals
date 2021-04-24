2f4f7d14861c8407563e821bbed667faab2e0fe1
giantpetter <65336996+giantpetter@users.noreply.github.com>
2021-04-24 14:51:52 +0800

    覆盖equals 方法

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -24,5 +24,7 @@ public class User {
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
-
+    public boolean equals(User obj) {
+        return this.getId().equals(obj.getId()) && this.getName().equals(obj.getName());
+    }
 }
