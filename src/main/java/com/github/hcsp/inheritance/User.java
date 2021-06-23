871700869605164388d585ebdddb24efba0e22b2
jianqibot <85292386+jianqibot@users.noreply.github.com>
2021-06-24 00:55:49 +1000

    job done (#221)
    job done (#221)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.inheritance;
 
-public class User {
+public class User<equals> {
     private Integer id;
     private String name;
 
@@ -18,11 +18,19 @@ public class User {
     }
 
     public static void main(String[] args) {
-        System.out.println(new User(1, "user1") == new User(1, "user1"));
+        System.out.println(new User(1, "") == new User(1, ""));
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
+        System.out.println(new User(1234, "").equals(new User(1234, "")));
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
-
+    @Override
+    public boolean equals (Object user) {
+        if (user == null) {
+            return false;
+        } else {
+            return this.id.intValue() == ((User) user).id.intValue();
+        }
+    }
 }
