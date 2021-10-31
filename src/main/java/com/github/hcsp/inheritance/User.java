05423db88d57d1cac8343381be21904b384af1b3
thomaszzzzz <84179177+thomaszzzzz@users.noreply.github.com>
2021-10-31 18:31:18 +0800

    User o1 = (User)o; (#237)
    User o1 = (User)o; (#237)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import java.util.Objects;
+
 public class User {
     private Integer id;
     private String name;
@@ -25,4 +27,14 @@ public class User {
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
 
+
+    @Override
+    public boolean equals(Object o) {
+        return o != null && this.getClass().equals(o.getClass()) && this.getId().equals(((User) o).getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
