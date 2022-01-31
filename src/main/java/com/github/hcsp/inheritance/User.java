3b4737520a11642fddc1c36e2e53b6638788b924
Yangbisheng <2606424906@qq.com>
2022-02-01 02:43:29 +0800

    覆写equlas 方法

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,6 +1,8 @@
 package com.github.hcsp.inheritance;
 
-public class User {
+import java.util.Objects;
+
+public class User extends Object {
     private Integer id;
     private String name;
 
@@ -24,5 +26,12 @@ public class User {
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            return Objects.equals(getId(), ((User) obj).getId());
+        }
+        return false;
 
+    }
 }
