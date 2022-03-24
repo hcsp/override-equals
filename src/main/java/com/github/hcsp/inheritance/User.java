f6e2006aa65d87395bfdb0956d5d5984a98f2ba9
LiDianguang <101635266+LiDianguang@users.noreply.github.com>
2022-03-25 00:50:53 +0800

    Update User.java

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -23,6 +23,12 @@ public class User {
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
     }
 
-    // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof User) {
+            return (this.getId() == ((User) obj).getId());
+        }
+        return false;
+    }
 
 }
