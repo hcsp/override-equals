dd62f91246d9000b4f1ac44d9c28d88b02f07cad
lx <iamxliu@163.com>
2022-04-05 09:17:12 +0800

    9

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,6 +1,6 @@
 package com.github.hcsp.inheritance;
 
-public class User {
+public class User extends Object {
     private Integer id;
     private String name;
 
@@ -25,9 +25,11 @@ public class User {
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
 
-//    @Override
-    public boolean equals(User obj) {
-        if (this.id == obj.id) {
+    @Override
+    public boolean equals(Object obj) {
+
+        User u = (User)obj;
+        if (this.id == u.getId()) {
             return true;
         } else {
             return false;
