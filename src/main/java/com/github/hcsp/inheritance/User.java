78387fbc7973f589947cf90c03dfded738c93201
RyanLin514 <101817394+RyanLin514@users.noreply.github.com>
2022-04-14 23:42:30 +0800

    我已完成覆盖equals方法的作业 (#259)
    我已完成覆盖equals方法的作业 (#259)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,8 +1,10 @@
 package com.github.hcsp.inheritance;
 
+import java.util.Objects;
+
 public class User {
-    private Integer id;
-    private String name;
+    private final Integer id;
+    private final String name;
 
     public User(Integer id, String name) {
         this.id = id;
@@ -18,11 +20,20 @@ public class User {
     }
 
     public static void main(String[] args) {
-        System.out.println(new User(1, "user1") == new User(1, "user1"));
+        System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id);
+    }
+
 
 }
