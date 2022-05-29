2cacadc3ad449f9cb104343d626dc96dc5032a29
3stonelei <1462070965@qq.com>
2022-05-29 20:07:02 +0800

    覆盖equals

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,9 @@
 package com.github.hcsp.inheritance;
 
+import org.graalvm.compiler.lir.LIRInstruction;
+
+import java.util.Objects;
+
 public class User {
     private Integer id;
     private String name;
@@ -25,4 +29,19 @@ public class User {
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
 
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        User user = (User) o;
+
+        return Objects.equals(id,user.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
