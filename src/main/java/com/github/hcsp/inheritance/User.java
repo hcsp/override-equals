eda9aad7a69a8742715f960112ee6f725fd329b5
yangbisheng <2606424906@qq.com>
2022-08-10 23:31:30 +0800

    override equals (#267)
    override equals (#267)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -1,5 +1,7 @@
 package com.github.hcsp.inheritance;
 
+import java.util.Objects;
+
 public class User {
     private Integer id;
     private String name;
@@ -21,6 +23,19 @@ public class User {
         System.out.println(new User(1, "user1") == new User(1, "user1"));
         System.out.println(new User(1, "user1").equals(new User(1, "user1")));
         System.out.println(new User(1, "user1").equals(new User(2, "user2")));
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return id.equals(user.id) && Objects.equals(name, user.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
