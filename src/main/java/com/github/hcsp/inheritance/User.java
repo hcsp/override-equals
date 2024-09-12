ec43dba782a139e72fcf631fc0f7e1431df59939
GJV123456 <161851858+GJV123456@users.noreply.github.com>
2024-09-12 16:11:05 +0800

    重写方法 (#278)
    重写方法 (#278)

--- src/main/java/com/github/hcsp/inheritance/User.java
+++ src/main/java/com/github/hcsp/inheritance/User.java
@@ -24,5 +24,21 @@ public class User {
     }
 
     // 请在这里覆盖equals方法，使得两个相同ID的用户equals返回true
+    @Override
+    public boolean equals(Object obj) {
+        // 首先检查是否为同一个对象
+        if (this == obj) {
+            return true;
+        }
+        // 检查对象是否为User类型
+        if (obj instanceof User) {
+            // 将 obj 对象转换为 User 类型。
+            User other = (User) obj;
+            // 比较两个User的id是否相同
+            return this.id != null && this.id.equals(other.id);
+        }
+        // 如果对象类型不同，返回false
+        return false;
+    }
 
 }
